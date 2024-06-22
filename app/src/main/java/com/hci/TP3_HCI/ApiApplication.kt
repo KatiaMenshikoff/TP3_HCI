e76d34065986b9d51438162abe7b3913464f8937
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-06-22 14:49:50 -0300

    Manifest & ApiApplication >Manifest: Actualizado para usar de nombre de aplicacion apiApplication >ApiApplication: Removi las lineas de Rooms

--- app/src/main/java/com/hci/TP3_HCI/ApiApplication.kt
+++ app/src/main/java/com/hci/TP3_HCI/ApiApplication.kt
@@ -2,21 +2,12 @@ package com.hci.TP3_HCI
 
 import android.app.Application
 import com.hci.TP3_HCI.remote.DeviceRemoteDataSource
-import com.hci.TP3_HCI.remote.RoomRemoteDataSource
 import com.hci.TP3_HCI.remote.api.RetrofitClient
 import com.hci.TP3_HCI.repository.DeviceRepository
-import com.hci.TP3_HCI.repository.RoomRepository
 
 class ApiApplication  : Application() {
-
-    private val roomRemoteDataSource: RoomRemoteDataSource
-        get() = RoomRemoteDataSource(RetrofitClient.roomService)
-
     private val deviceRemoteDataSource: DeviceRemoteDataSource
         get() = DeviceRemoteDataSource(RetrofitClient.deviceService)
-
-    val roomRepository: RoomRepository
-        get() = RoomRepository(roomRemoteDataSource)
 
     val deviceRepository: DeviceRepository
         get() = DeviceRepository(deviceRemoteDataSource)

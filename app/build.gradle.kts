305e9993223253d0bc31a64172ca38b0e1747261
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-06-22 11:07:54 -0300

    build.gradle y gradle/libs.versions >build.gradle: Implemente las librerias importantes para usar la api (tp ejemplo) >gradle/libs.versions.toml: Declare las dependencias necesarias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-//    alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
 android {
@@ -66,9 +65,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-//    implementation(libs.converter.kotlin.serializtion)
-//    implementation(libs.retrofit)
-    implementation(libs.okhttp)
     implementation(libs.logging.interceptor)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.material)
@@ -81,4 +77,17 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //api
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.gson)
+    implementation(libs.converter.gson)
+    implementation(libs.retrofit)
+
+    //extra
+    implementation(libs.androidx.material.icons.extended)
+
 }

a2e8aa7de0d86ac19a960396ec16e3b57810d2c5
kmenshikoff <kmenshikoff@itba.edu.ar>
2024-06-23 20:55:52 -0300

    updated sprinkler

--- app/src/main/java/com/hci/TP3_HCI/ui/sprinkler/SprinklerScreen.kt
+++ app/src/main/java/com/hci/TP3_HCI/ui/sprinkler/SprinklerScreen.kt
@@ -1,27 +1,27 @@
 package com.hci.TP3_HCI.ui.sprinkler
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hci.TP3_HCI.R
-import com.hci.TP3_HCI.ui.getViewModelFactory
-import com.hci.TP3_HCI.ui.lamp.LampViewModel
 
 @Composable
 fun SprinklerScreen(
@@ -30,25 +30,139 @@ fun SprinklerScreen(
 ) {
     viewModel.setCurrentDevice(deviceId)
     val uiState by viewModel.uiState.collectAsState()
-    Scaffold() { paddingValues ->
+    var quantity by remember { mutableStateOf(0) }
+    var unit by remember { mutableStateOf("ml") }
+    var expanded by remember { mutableStateOf(false) }
+    var showInHome by remember { mutableStateOf(false) }
+
+    Scaffold { paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
                 .background(colorResource(R.color.background))
-                .padding(3.dp)
+                .padding(16.dp)
         ) {
-            val currentDeviceData = uiState.currentDevice?.let {
-                "(${it.id}) ${it.name}"
-            } ?: stringResource(R.string.unknown)
+            // Title
             Text(
-                text = stringResource(R.string.current_device, currentDeviceData),
+                text = stringResource(id = R.string.sprinkler_title),
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            // Device Name and Status
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 16.dp, start = 16.dp, end = 16.dp),
-                fontSize = 18.sp
+                    .padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "sprinkler", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                Icon(painter = painterResource(id = R.drawable.icon_sprinkler), contentDescription = "Sprinkler Icon", tint = Color.Gray)
+            }
+            Text(
+                text = stringResource(id = R.string.current_status, "closed"),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Gray,
+                modifier = Modifier.padding(vertical = 8.dp)
             )
+
+            // Pump Water Controls
             Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = stringResource(id = R.string.pump_water), fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                Icon(painter = painterResource(id = R.drawable.icon_water), contentDescription = "Water Icon", tint = Color.Gray)
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextField(
+                    value = quantity.toString(),
+                    onValueChange = { quantity = it.toIntOrNull() ?: 0 },
+                    label = { Text(text = stringResource(id = R.string.quantity)) },
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Box(modifier = Modifier.weight(1f)) {
+                    TextField(
+                        value = unit,
+                        onValueChange = { unit = it },
+                        label = { Text(text = stringResource(id = R.string.unit)) },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable { expanded = true }
+                    )
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        DropdownMenuItem(onClick = {
+                            unit = "ml"
+                            expanded = false
+                        }) {
+                            Text(text = "ml")
+                        }
+                        DropdownMenuItem(onClick = {
+                            unit = "l"
+                            expanded = false
+                        }) {
+                            Text(text = "l")
+                        }
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = { /* handle dispense water */ },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = stringResource(id = R.string.dispense_water), fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            }
+
+            // Show in Home
+            Spacer(modifier = Modifier.height(32.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = showInHome,
+                    onCheckedChange = { showInHome = it }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = stringResource(id = R.string.show_in_home), fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            }
+
+            // Delete Button
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(
+                onClick = { /* handle delete */ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red
+                ),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(painter = painterResource(id = R.drawable.icon_delete), contentDescription = stringResource(id = R.string.delete), tint = Color.White)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = stringResource(id = R.string.delete), color = Color.White, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SprinklerScreenPreview() {
+    SprinklerScreen(
+        deviceId = "1",
+    )
 }

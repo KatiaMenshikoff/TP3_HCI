8ced6e3a860cba104ff67f6d201d90108f4612da
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-06-23 23:38:04 -0300

    borre paginas de la bottombar

--- app/src/main/java/com/hci/TP3_HCI/ui/component/AppBottomBar.kt
+++ app/src/main/java/com/hci/TP3_HCI/ui/component/AppBottomBar.kt
@@ -21,14 +21,11 @@ fun AppBottomBar(
         AppDestinations.HOME to ImageVector.vectorResource(id = R.drawable.icon_home),
         AppDestinations.DEVICES to ImageVector.vectorResource(id = R.drawable.icon_device),
         AppDestinations.SETTINGS to ImageVector.vectorResource(id = R.drawable.icon_settings),
-        AppDestinations.LAMP to ImageVector.vectorResource(id = R.drawable.icon_lamp)
     )
 
     val items = listOf(
-        AppDestinations.HOME,
         AppDestinations.DEVICES,
         AppDestinations.SETTINGS,
-        AppDestinations.LAMP
     )
 
     NavigationBar {
@@ -43,27 +40,3 @@ fun AppBottomBar(
         }
     }
 }
-
-//@Composable
-//fun AppBottomBar(
-//    currentRoute: String?,
-//    onNavigateToRoute: (String) -> Unit
-//) {
-//    val items = listOf(
-//        AppDestinations.HOME,
-//        AppDestinations.DEVICES,
-//        AppDestinations.LAMP
-//    )
-//
-//    NavigationBar {
-//        items.forEach { item ->
-//            NavigationBarItem(
-//                icon = { Icon(imageVector = item.icon, contentDescription = stringResource(item.title)) },
-//                label = { Text(text = stringResource(item.title)) },
-//                alwaysShowLabel = true,
-//                selected = currentRoute == item.route,
-//                onClick = { onNavigateToRoute(item.route) }
-//            )
-//        }
-//    }
-//}

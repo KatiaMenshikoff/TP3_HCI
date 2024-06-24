044938b69dfcd325c57169bd9a3e29a375d2cba2
kmenshikoff <kmenshikoff@itba.edu.ar>
2024-06-23 23:46:24 -0300

    added device card

--- app/src/main/java/com/hci/TP3_HCI/ui/component/DeviceCard.kt
+++ app/src/main/java/com/hci/TP3_HCI/ui/component/DeviceCard.kt
@@ -5,23 +5,21 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hci.TP3_HCI.R
 import com.hci.TP3_HCI.model.Device
-import com.hci.TP3_HCI.ui.navigation.AppDestinations
+import com.hci.TP3_HCI.model.DeviceType
+
 
 @Composable
 fun DeviceCard(
@@ -29,6 +27,13 @@ fun DeviceCard(
     onSelectDevice: () -> Unit,
     onNavigateToDevice: () -> Unit,
 ) {
+    val deviceTypeNames = mapOf(
+        DeviceType.LAMP to R.string.views_lamp_name,
+        DeviceType.AC to R.string.views_ac_name,
+        DeviceType.SPEAKER to R.string.views_speaker_name,
+        DeviceType.SPRINKLER to R.string.views_sprinkler_name
+    )
+
     Card(
         shape = RoundedCornerShape(12.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
@@ -60,42 +65,15 @@ fun DeviceCard(
                     style = MaterialTheme.typography.titleMedium
                 )
                 Spacer(modifier = Modifier.height(4.dp))
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.icon_minus),
-                        contentDescription = null,
-                        tint = Color.White,
-                        modifier = Modifier.size(20.dp)
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(
-                        text = device.id!!,
-                        color = Color.White,
-                        fontSize = 16.sp,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-                }
-            }
-            Column(
-                verticalArrangement = Arrangement.SpaceBetween,
-                horizontalAlignment = Alignment.End
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.icon_device),
-                    contentDescription = null,
-                    tint = Color.White,
-                    modifier = Modifier.size(20.dp)
+                Text(
+                    text = stringResource(id = deviceTypeNames[device.type] ?: R.string.unknown),
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    style = MaterialTheme.typography.bodyMedium
                 )
-                Spacer(modifier = Modifier.height(8.dp))
+
             }
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun CardPreview() {
-
-}
